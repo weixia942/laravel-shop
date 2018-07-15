e681c11a1120c92ed4a087a0c42d7cf1d2367bf4
vagrant <vagrant@homestead>
2018-07-15 00:12:56 +0000

    新增收货地址

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,6 +7,8 @@
 
 require('./bootstrap');
 
+
+
 window.Vue = require('vue');
 
 /**
@@ -16,6 +18,10 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+require('./components/SelectDistrict');
+
+require('./components/UserAddressesCreateAndEdit');
 
 const app = new Vue({
     el: '#app'
