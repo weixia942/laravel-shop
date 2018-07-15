fcfab9ef7f6943895089278c635db9f4c9c86c42
vagrant <vagrant@homestead>
2018-07-15 13:25:37 +0000

    修改和删除收货地址

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\UserAddress;
+use App\Policies\UserAddressPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -12,9 +14,9 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array
      */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+     protected $policies = [
+         UserAddress::class => UserAddressPolicy::class,
+     ];
 
     /**
      * Register any authentication / authorization services.
